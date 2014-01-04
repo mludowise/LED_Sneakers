bdae63125e111e0f28a101f7f46fa8ce4a0b678a
mludowise <mludowise@gmail.com>
2014-01-04 14:44:11 -0800

    Abstracted code to Button and Animation classes

--- LED_Sneakers.ino
+++ LED_Sneakers.ino
@@ -20,90 +20,180 @@
 #define WAIT 25             // Time delay to wait for LEDs to light up
 
 // Constants used to calculate rainbow effect
-const float RAINBOW_EFFECT_FREQUENCY = 2 * PI / NUM_LEDS;  // Use this frequency when displaying rainbow effect. The shoe will show one rainbow cycle around its circumfrence.
-const float SINGLE_COLOR_FREQUENCY = 2 * PI / NUM_COLORS;  // Use this frequency to calculate color of the LEDs when cycling through the colors.
-const float PI2_3 = 2 * PI / 3;
-const float PI4_3 = 4 * PI / 3;
+static const float RAINBOW_EFFECT_FREQUENCY = 2 * PI / NUM_LEDS;  // Use this frequency when displaying rainbow effect. The shoe will show one rainbow cycle around its circumfrence.
+static const float SINGLE_COLOR_FREQUENCY = 2 * PI / NUM_COLORS;  // Use this frequency to calculate color of the LEDs when cycling through the colors.
+static const float PI2_3 = 2 * PI / 3;
+static const float PI4_3 = 4 * PI / 3;
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_OUTPUT, NEO_GRB + NEO_KHZ800);
-int rainbowShift = 0;                // this variable will shift the color that the rainbow effect starts with each time it's stepped on
-int currentColor = 0;                // denotes the current color of the LEDs, cycled from 0 to NUM_COLORS
-boolean colorButtonPressed = false;  // current state of the color button (true = pressed)
-boolean isAnimating = false;         // is the shoe in the process of a swipe animation?
-boolean turnLEDsOn = true;           // is the next step in the animation to turn the LEDs on or off?
-int nextLED = 0;                     // the index of the next LED in the animation
-unsigned long startedWaiting;
+class Button {
+  private:
+    Button(const Button& copy); 
+    Button& operator=(const Button& copy); 
+
+    const int mPin;
+    const int mThreshold;
+    bool mIsDown;
+  
+  protected:
+    Button(int pin, int input, int threshold) :
+      mPin(pin), mThreshold(threshold), mIsDown(false) {
+        pinMode(input, INPUT_PULLUP);
+      }
+      
+    virtual void onDown() = 0;
+    virtual void onUp() = 0;
+    
+  public:
+    void checkButtonState() {
+      int sensorValue = analogRead(mPin);
+      if (sensorValue < mThreshold) { // Pressed
+        bool wasDown = mIsDown;
+        mIsDown = true;
+        if (!wasDown) {
+          onDown();
+        }
+      } else if (mIsDown) { // Released
+        mIsDown = false;
+        onUp();
+      }
+    }
+    
+    bool isDown() const {
+      return mIsDown;
+    }
+};
+
+class ShoeAnimation {
+  private:
+    ShoeAnimation(const ShoeAnimation& copy); 
+    ShoeAnimation& operator=(const ShoeAnimation& copy); 
+    
+    Adafruit_NeoPixel mStrip;
+    bool mIsAnimating; 
+    bool mTurnLEDsOn;
+    int mNextLEDIndex;
+    unsigned long mStartedWaiting;
+    int mRainbowShift;
+    uint32_t mColor;
+    
+  public: 
+    ShoeAnimation() : 
+      mStrip(NUM_LEDS, LED_OUTPUT, NEO_GRB + NEO_KHZ800), mIsAnimating(false), mTurnLEDsOn(true), mNextLEDIndex(0), mRainbowShift(0), mColor(0) {
+        mStrip.begin();
+        mStrip.show(); // Initialize all pixels to 'off'
+      }
+      
+      void startAnimation() {
+        mIsAnimating = true;
+        mTurnLEDsOn = true;
+        mNextLEDIndex = 0;
+        mStartedWaiting = 0;
+        if (mColor == 0) { // Currently displaying rainbow
+          ++mRainbowShift %= NUM_LEDS;     // Shift the rainbow for the next step
+        }
+        incrementAnimation();
+      }
+      
+      void incrementAnimation() {
+        unsigned long now = millis();
+        if (mStartedWaiting + WAIT <= now) { // If we've been waiting long enough for the next step in the animation
+          changeNextLED();
+          mStartedWaiting = now;
+        }
+      }
+      
+      void changeNextLED() {
+        uint32_t nextLEDColor = 0;
+        if (mTurnLEDsOn) {
+          if (mColor == 0) {
+            nextLEDColor = calculateColor(mNextLEDIndex + mRainbowShift, RAINBOW_EFFECT_FREQUENCY, 0);
+          } else {
+            nextLEDColor = mColor;
+          }
+        }
+  
+        #if TEST == 1
+        Serial.print((mColor & 0xFF0000) >> 16);
+        Serial.print("\t");
+        Serial.print((mColor & 0x00FF00) >> 8);
+        Serial.print("\t");
+        Serial.println(mColor & 0x0000FF);
+        #endif
+  
+        mStrip.setPixelColor((mNextLEDIndex + FIRST_LED) % NUM_LEDS, nextLEDColor);
+        mStrip.show();
+  
+        mNextLEDIndex++; // Increment LED
+        if (mNextLEDIndex >= NUM_LEDS) {
+          if (mTurnLEDsOn) {
+            mNextLEDIndex = 0;
+            mTurnLEDsOn = false;
+          } else {
+            mIsAnimating = false;
+          }
+        }
+      }
+      
+      bool isAnimating() const {
+        return mIsAnimating;
+      }
+      
+      void setColor(uint32_t c) {
+        mColor = c;
+      }
+};
+
+ShoeAnimation* animation = NULL;
+
+class ColorButton : public Button {
+  private:
+    const int mNumColors;
+    const float mFrequency;
+    const float mColorOffset;
+    int mCurrentColorIndex;
+    ShoeAnimation* mAnimationPtr;
+    
+  public:
+    ColorButton(int pin, int input, int threshold, int numColors, float colorOffset, ShoeAnimation* animationPtr) : 
+      Button (pin, input, threshold), mNumColors(numColors), mColorOffset(colorOffset), mFrequency(2 * PI / numColors), mCurrentColorIndex(0), mAnimationPtr(animationPtr) {
+        updateAnimationColor();
+      }
+  
+  private:
+    void onUp() {
+      ++mCurrentColorIndex %= (mNumColors + 1);
+      updateAnimationColor();
+    }
+    
+    void onDown() { }
+    
+    void updateAnimationColor() {
+      bool isRainbow = mCurrentColorIndex == mNumColors;
+      if (mAnimationPtr) {
+        mAnimationPtr->setColor(isRainbow ? 0 : calculateColor(mCurrentColorIndex, mFrequency, mColorOffset));
+      }
+    }
+};
+
+ColorButton* colorButton = NULL;
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
   pinMode(STEP_INPUT, INPUT_PULLUP);
-  pinMode(COLOR_INPUT, INPUT_PULLUP);
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  
+  animation = new ShoeAnimation();
+  colorButton = new ColorButton(COLOR_IN_PIN, COLOR_INPUT, 100, NUM_COLORS, COLOR_OFFSET, animation);
 }
  
 void loop() {
   // Check for color change
-  checkForColorChange();
-  
-  #if 0
-  Serial.print("isAnimating = ");
-  Serial.print(isAnimating);
-  Serial.print("\tturnLEDsOn = ");
-  Serial.print(turnLEDsOn);
-  Serial.print("\tnextLED = ");
-  Serial.print(nextLED);
-  Serial.print("\tstartedWaiting = ");
-  Serial.println(startedWaiting);
-  #endif
-  
-  if (isAnimating) { // Are we in the process of an LED animation?
-    Serial.print("Animating!");
-    unsigned long now = millis();
-    if (startedWaiting + WAIT <= now) { // If we've been waiting long enough for the next step in the animation
-      changeNextLED();
-      startedWaiting = now;
-      Serial.print("\tWaited long enough");
-    }
-    Serial.println();
+  colorButton->checkButtonState();
+    
+  if (animation->isAnimating()) { // Are we in the process of an LED animation?
+    animation->incrementAnimation();
   } else if (checkIfStepping()) { // Take another step?
-    Serial.println("Stepped!");
-    isAnimating = true;
-    turnLEDsOn = true;
-    nextLED = 0;
-    if (currentColor == NUM_COLORS) { // Currently displaying rainbow
-      ++rainbowShift %= NUM_LEDS;     // Shift the rainbow for the next step
-    }
-    changeNextLED();
-    startedWaiting = millis();
-  }
-}
-  
-void checkForColorChange() {
-  int colorSensorValue = analogRead(COLOR_IN_PIN);
-  
-  #if TEST == 1
-  Serial.print("\tbutton = ");
-  Serial.println(colorSensorValue);
-  #endif
-  
-  // Determine if color button has been pressed
-  if (colorSensorValue < 100) { // Pressed
-    colorButtonPressed = true;
-    
-    #if TEST == 1
-    Serial.println("Button Pressed!");
-    #endif
-  } else if (colorButtonPressed) { // Depressed, change color
-    colorButtonPressed = false;
-    ++currentColor %= (NUM_COLORS + 1);
-    
-    #if TEST
-    Serial.println("Button Released!");
-    Serial.print("Color Index = ");
-    Serial.println(currentColor);
-    #endif
+    animation->startAnimation();
   }
 }
 
@@ -118,62 +208,9 @@ boolean checkIfStepping() {
   return stepSensorValue < STEP_SENSOR_TRIGGER;
 }
 
-void changeNextLED() {
-  uint32_t color = 0;
-  if (turnLEDsOn) {
-    Serial.print("turning on LED ");
-    Serial.print(nextLED);
-    if (currentColor == NUM_COLORS) { // Rainbow
-      color = calculateColor(nextLED + rainbowShift, RAINBOW_EFFECT_FREQUENCY, 0);
-      Serial.println(" to rainbow");
-    } else { // Single color
-      color = calculateColor(currentColor, SINGLE_COLOR_FREQUENCY, COLOR_OFFSET);
-      Serial.println(" to color");
-    }
-  }
-  
-  #if TEST == 1
-  Serial.print((color & 0xFF0000) >> 16);
-  Serial.print("\t");
-  Serial.print((color & 0x00FF00) >> 8);
-  Serial.print("\t");
-  Serial.println(color & 0x0000FF);
-  #endif
-  
-  strip.setPixelColor((nextLED + FIRST_LED) % NUM_LEDS, color);
-  strip.show();
-  
-  nextLED++; // Increment LED
-  if (nextLED >= NUM_LEDS) {
-    if (turnLEDsOn) {
-      nextLED = 0;
-      turnLEDsOn = false;
-    } else {
-      isAnimating = false;
-    }
-  }
-}
-
-/*
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i = 0; i < NUM_LEDS; i++) {
-    strip.setPixelColor((i + FIRST_LED) % NUM_LEDS, c);
-    strip.show();
-    delay(wait);
-  }
-}
-
-void rainbowWipe(int colorShift, uint8_t wait) {
-  for(uint16_t i = 0; i < NUM_LEDS; i++) {
-    strip.setPixelColor((i + FIRST_LED) % NUM_LEDS, calculateColor(i + colorShift, RAINBOW_EFFECT_FREQUENCY, 0));
-    strip.show();
-    delay(wait);
-  }
-}
-*/
 // Referenced from http://krazydad.com/tutorials/makecolors.php
 uint32_t calculateColor(int i, float frequency, float offset) {
- return strip.Color(
+ return Adafruit_NeoPixel::Color(
       sin(frequency * (i + offset)) * 127 + 128,
       sin(frequency * (i + offset) + PI2_3) * 127 + 128,
       sin(frequency * (i + offset) + PI4_3) * 127 + 128
